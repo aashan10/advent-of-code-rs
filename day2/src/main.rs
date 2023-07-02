014336ded3d2be57f3df6e3e6b583874e7dd5a42
Aashan Ghimire <ashanghimire10@gmail.com>
2023-07-02 11:00:38 +0545

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -18,18 +18,22 @@ fn main() {
     let results = read_file();
 
     let mut score = 0;
+    let mut score2 = 0;
     for result in results {
         let points = get_points(result);
+        let points2 = get_result_point((get_rock_paper_scissors_from_char(result.0), result.1));
+        score2 += points2;
         score += points;
     }
 
     println!("Score: {}", score);
+    println!("Score2: {}", score2);
 }
 
-fn read_file() -> Vec<(RockPaperScissors, RockPaperScissors)> {
+fn read_file() -> Vec<(char, char)> {
     let file_path = env::current_dir().unwrap();
     let p = file_path.join("src/cheatcode.txt");
-    let mut result: Vec<(RockPaperScissors, RockPaperScissors)> = Vec::new();
+    let mut result: Vec<(char, char)> = Vec::new();
     
 
     let contents = fs::read_to_string(p)
@@ -38,8 +42,8 @@ fn read_file() -> Vec<(RockPaperScissors, RockPaperScissors)> {
     for line in contents.lines() {
         let chunks = line.split(" ").collect::<Vec<&str>>();
 
-        let player1 = get_rock_paper_scissors_from_char(chunks[0].chars().nth(0).unwrap());
-        let player2 = get_rock_paper_scissors_from_char(chunks[1].chars().nth(0).unwrap());
+        let player1 = chunks[0].chars().nth(0).unwrap();
+        let player2 = chunks[1].chars().nth(0).unwrap();
 
         result.push((player1, player2));
     }
@@ -57,7 +61,12 @@ fn get_rock_paper_scissors_from_char(c: char) -> RockPaperScissors {
     }
 }
 
-fn get_points((player1, player2): (RockPaperScissors, RockPaperScissors)) -> usize {
+
+fn get_points((player1, player2): (char, char)) -> usize {
+    
+    let player1 = get_rock_paper_scissors_from_char(player1);
+    let player2 = get_rock_paper_scissors_from_char(player2);
+
     let point = match player2 {
         RockPaperScissors::Rock => 1,
         RockPaperScissors::Paper => 2,
@@ -77,4 +86,55 @@ fn get_points((player1, player2): (RockPaperScissors, RockPaperScissors)) -> usi
     };
 
     point + winner_point
+}
+
+
+fn get_points_if_second_column_is_result_type(results: Vec<(char, char)>) -> usize {
+
+    let mut score = 0;
+    for result in results {
+        let first = get_rock_paper_scissors_from_char(result.0);
+
+        let second = result.1;
+
+        score += get_result_point((first, second));
+    }
+
+    score
+}
+
+fn get_result_point((player, result): (RockPaperScissors, char)) -> usize {
+    
+    let win_point = match result {
+        'X' => 0,
+        'Y' => 3,
+        'Z' => 6,
+        _ => 0,
+    };
+
+    let player_move = match (player, result) {
+        (RockPaperScissors::Rock, 'X') => RockPaperScissors::Scissors,
+        (RockPaperScissors::Rock, 'Y') => RockPaperScissors::Rock,
+        (RockPaperScissors::Rock, 'Z') => RockPaperScissors::Paper,
+
+
+        (RockPaperScissors::Paper, 'X') => RockPaperScissors::Rock,
+        (RockPaperScissors::Paper, 'Y') => RockPaperScissors::Paper,
+        (RockPaperScissors::Paper, 'Z') => RockPaperScissors::Scissors,
+
+
+        (RockPaperScissors::Scissors, 'X') => RockPaperScissors::Paper,
+        (RockPaperScissors::Scissors, 'Y') => RockPaperScissors::Scissors,
+        (RockPaperScissors::Scissors, 'Z') => RockPaperScissors::Rock,
+
+        _ => { panic!("Invalid character")}
+    };
+
+    let point = match player_move {
+        RockPaperScissors::Rock => 1,
+        RockPaperScissors::Paper => 2,
+        RockPaperScissors::Scissors => 3,
+    };
+
+    win_point + point
 }
